4a314a753721371f30a293b669295b738598a228
Nguessan007 <enguessan@byupathway.edu>
2025-09-07 06:19:17 +0000

    f

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name?");
+        string first= Console.Read();
+
+        Console.Write("What is your last name?");
+        string last= Console.Read();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
