846dd3171a42510f5ec411587d086c20d72d745a
Nguessan007 <enguessan@byupathway.edu>
2025-09-21 01:18:19 +0000

    j

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using FractionsApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction(1, 2);   // 1/2
+        Fraction f2 = new Fraction(3, 4);   // 3/4
+
+        Console.WriteLine($"Fraction 1: {f1} = {f1.ToDecimal()}");
+        Console.WriteLine($"Fraction 2: {f2} = {f2.ToDecimal()}");
+
+        Console.WriteLine("\nArithmetic:");
+        Console.WriteLine($"{f1} + {f2} = {f1.Add(f2)}");
+        Console.WriteLine($"{f1} - {f2} = {f1.Subtract(f2)}");
+        Console.WriteLine($"{f1} * {f2} = {f1.Multiply(f2)}");
+        Console.WriteLine($"{f1} / {f2} = {f1.Divide(f2)}");
     }
 }
